f2560d99bb5e0f343bbf401d37cb1e56472dc67a
weslleysilv4 <weslleysilva.swe@gmail.com>
2025-10-04 17:37:42 -0300

    feat: layout component to integrate font loading and improve theme provider usage

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
 import '@/global.css';
 
 import { NAV_THEME } from '@/lib/theme';
-import { ThemeProvider } from '@react-navigation/native';
+import { ThemeProvider as NavigationThemeProvider } from '@react-navigation/native';
+import { ThemeProvider } from '@/components/theme-provider';
 import { PortalHost } from '@rn-primitives/portal';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from 'nativewind';
+import { useFonts } from 'expo-font';
+import { Manrope_400Regular, Manrope_500Medium, Manrope_700Bold } from '@expo-google-fonts/manrope';
 
 export const unstable_settings = {
   initialRouteName: '(tabs)',
@@ -14,14 +17,26 @@ export const unstable_settings = {
 export default function RootLayout() {
   const { colorScheme } = useColorScheme();
 
+  const [fontsLoaded] = useFonts({
+    Manrope_400Regular,
+    Manrope_500Medium,
+    Manrope_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <ThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
-      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ headerShown: false }} />
-      </Stack>
-      <PortalHost />
+    <ThemeProvider>
+      <NavigationThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
+        <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ headerShown: false }} />
+        </Stack>
+        <PortalHost />
+      </NavigationThemeProvider>
     </ThemeProvider>
   );
 }
